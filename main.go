3562b99d2cf78031efdbddf0fff66d96e5181aa7
snimmag6 <sai.nimmagadda@optum.com>
2019-03-17 20:47:48 -0400

    Can now create Azure resource group terraform definition

--- main.go
+++ main.go
@@ -1,50 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
 
-	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
-	"github.com/snimmagadda1/azure-terraform-generator/pkg/azureauth"
 	"github.com/snimmagadda1/azure-terraform-generator/pkg/azureresources"
-
-	"github.com/pkg/errors"
 )
-
-func getGroups(sess *azureauth.AzureSession) ([]string, error) {
-	tab := make([]string, 0)
-	var err error
-
-	grClient := resources.NewGroupsClient(sess.SubscriptionID)
-	grClient.Authorizer = sess.Authorizer
-
-	for list, err := grClient.ListComplete(context.Background(), "", nil); list.NotDone(); err = list.Next() {
-		if err != nil {
-			return nil, errors.Wrap(err, "error traverising RG list")
-		}
-		rgName := *list.Value().Name
-		tab = append(tab, rgName)
-	}
-	return tab, err
-}
 
 func main() {
 	fmt.Println("Development in progress!")
-	azureresources.TestTemplate()
 
-	os.Setenv("AZURE_AUTH_LOCATION", "/Users/snimmag6/go/src/github.com/snimmagadda1/azure-terraform-generator/my.auth")
+	azureresources.CreateTerraResourceGroup("testResourceGroup1")
 
-	sess, err := azureauth.NewSessionFromFile()
-
-	groups, err := getGroups(sess)
-
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
-
-	for _, group := range groups {
-		fmt.Println(group)
-	}
 }
--- pkg/azureresources/resourceGroup.go
+++ pkg/azureresources/resourceGroup.go
@@ -1,15 +1,19 @@
 package azureresources
 
 import (
+	"context"
 	"os"
 	"text/template"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/resources"
+	"github.com/snimmagadda1/azure-terraform-generator/pkg/azureauth"
 )
 
 // ResourceGroup is an Azure resource group
 type ResourceGroup struct {
 	Name     string
 	Location string
-	Tags     map[string]string
+	Tags     map[string]*string
 }
 
 // TODO: text/template support for tags
@@ -20,16 +24,50 @@ resource "azurerm_resource_group" "{{.Name}}" {
 }
 `
 
-// TestTemplate is a test function for use during development. It will be removed
-// TODO: Move to test package
-func TestTemplate() {
-	testGroup := ResourceGroup{"testName", "testLocation", nil}
+// createGroupsClient returns a new client for interacting w/ Azure resources & resource groups
+func createGroupsClient(sess *azureauth.AzureSession) resources.GroupsClient {
+	groupsClient := resources.NewGroupsClient(sess.SubscriptionID)
+	groupsClient.Authorizer = sess.Authorizer
+	return groupsClient
+}
+
+// getGroup gets info on the resource group in use
+func getGroup(ctx context.Context, sess *azureauth.AzureSession, groupName string) (resources.Group, error) {
+	groupsClient := createGroupsClient(sess)
+	return groupsClient.Get(ctx, groupName)
+}
+
+// getResourceGroup returns the above struct definition of a ResourceGroup
+func getResourceGroup(resourceGroup string) ResourceGroup {
+	sess, err := azureauth.NewSessionFromFile()
+
+	if err != nil {
+		panic(err)
+	}
+
+	returnedGroup, err := getGroup(context.Background(), sess, resourceGroup)
+
+	if err != nil {
+		panic(err)
+	}
+
+	group := ResourceGroup{Name: *returnedGroup.Name, Location: *returnedGroup.Location, Tags: returnedGroup.Tags}
+	return group
+}
+
+// CreateTerraResourceGroup creates a terraform resource defining the desired azure resource group
+// TODO: Handle map[string]*string in templating
+func CreateTerraResourceGroup(name string) {
+
+	resourceGroup := getResourceGroup(name)
+
 	tmpl, err := template.New("test").Parse(resourceTemplate)
 	if err != nil {
 		panic(err)
 	}
-	err = tmpl.Execute(os.Stdout, testGroup)
+	err = tmpl.Execute(os.Stdout, resourceGroup)
 	if err != nil {
 		panic(err)
 	}
+
 }
